0ee3c2fa999af4cdcd4099fb50cbfefd41ccd163
MfKrp <68623283+MfKrp@users.noreply.github.com>
2023-08-25 23:15:46 -0300

    Add files via upload

---
+++ Practico3/Pequeño Formulario.cs
@@ -0,0 +1,152 @@
+﻿using Proyecto3._0.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+        // keypress es un evento que detecta las teclas que son presionadas en el textbox.
+        private void TDni_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if((e.KeyChar >= 32 && e.KeyChar <=47) || (e.KeyChar >= 58 && e.KeyChar <= 255))
+            {
+                MessageBox.Show("Solamente se pueden ingresar números", "Alerta", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                e.Handled = true;
+                return;
+            }
+        }
+
+        private void TApellido_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TApellido_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar >= 32 && e.KeyChar <= 64) || (e.KeyChar >= 91 && e.KeyChar <= 96) || (e.KeyChar >= 123 && e.KeyChar <= 255))
+            {
+                MessageBox.Show("Solamente se pueden letras", "Alerta", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                e.Handled = true;
+                return;
+            }
+        }
+
+        private void TNombre_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar >= 32 && e.KeyChar <= 64) || (e.KeyChar >= 91 && e.KeyChar <= 96) || (e.KeyChar >= 123 && e.KeyChar <= 255))
+            {
+                MessageBox.Show("Solamente se pueden letras", "Alerta", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                e.Handled = true;
+                return;
+            }
+        }
+
+        private void BEliminar_Click(object sender, EventArgs e)
+        {
+
+
+            DialogResult ask = MessageBox.Show("Está a punto de eliminar el cliente:" + TNombre.Text + " " + TApellido.Text + '"', "Confirmar Eliminación", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button2);
+            
+            if(ask == DialogResult.Yes)
+            {
+                TDni.Clear();
+                TApellido.Clear();
+                TNombre.Clear();
+                TTelefono.Clear();
+                RBVaron.Checked = true;
+
+                LModificar.ForeColor = Color.Red;
+            }
+        
+        }
+        
+        private void BGuardar_Click(object sender, EventArgs e)
+        {
+
+            
+
+
+            if (string.IsNullOrWhiteSpace(TApellido.Text) || string.IsNullOrWhiteSpace(TDni.Text) || string.IsNullOrWhiteSpace(TNombre.Text) || string.IsNullOrWhiteSpace(TTelefono.Text))
+            {
+                MessageBox.Show("Debe completar todos los campos", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+             
+            }
+            else
+            {
+                LModificar.ForeColor = Color.Black;
+
+                // la variable tipo dialogResult devuelve true or false
+                DialogResult ask = MessageBox.Show("Seguro que desea insertar un nuevo Cliente?", "Confirmar Inserción", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1);
+
+                if(ask == DialogResult.Yes) {
+                    MessageBox.Show("El cliente: " + TNombre.Text + " " + TApellido.Text + " " + "se insertó correctamente", "Guardar", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+
+            }
+        }
+
+        private void Bsalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void TTelefono_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar >= 32 && e.KeyChar <= 47) || (e.KeyChar >= 58 && e.KeyChar <= 255))
+            {
+                MessageBox.Show("Solamente se pueden ingresar números", "Alerta", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                e.Handled = true;
+                return;
+            }
+        }
+
+        private void RBVaron_CheckedChanged(object sender, EventArgs e)
+        {
+            pictureBox1.BackgroundImage = null;
+
+            if(RBVaron.Checked == true)
+            {
+                String rutaVaron = "C:\\Users\\Usuario\\source\\repos\\Proyecto3.0\\Resources\\hombre.png";
+                pictureBox1.BackgroundImage = Image.FromFile(rutaVaron);
+            }
+        }
+
+        private void RBMujer_CheckedChanged(object sender, EventArgs e)
+        {
+            pictureBox1.BackgroundImage = null;
+
+            if (RBMujer.Checked == true)
+            {
+                String rutaMujer = "C:\\Users\\Usuario\\source\\repos\\Proyecto3.0\\Resources\\mujer.png";
+                pictureBox1.BackgroundImage = Image.FromFile(rutaMujer);
+            }
+        }
+    }
+}
---
+++ Practico3/Pequeño Formulario.designer.cs
@@ -0,0 +1,342 @@
+﻿namespace Proyecto3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LDni = new System.Windows.Forms.Label();
+            this.LNombre = new System.Windows.Forms.Label();
+            this.LApellido = new System.Windows.Forms.Label();
+            this.LNya = new System.Windows.Forms.Label();
+            this.LModificar = new System.Windows.Forms.Label();
+            this.TDni = new System.Windows.Forms.TextBox();
+            this.TApellido = new System.Windows.Forms.TextBox();
+            this.TNombre = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.LTarjeta = new System.Windows.Forms.Label();
+            this.TTelefono = new System.Windows.Forms.TextBox();
+            this.LTelefono = new System.Windows.Forms.Label();
+            this.RBVaron = new System.Windows.Forms.RadioButton();
+            this.RBMujer = new System.Windows.Forms.RadioButton();
+            this.Bsalir = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.BEliminar = new System.Windows.Forms.Button();
+            this.BGuardar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // LDni
+            // 
+            this.LDni.AutoSize = true;
+            this.LDni.Location = new System.Drawing.Point(38, 69);
+            this.LDni.Name = "LDni";
+            this.LDni.Size = new System.Drawing.Size(23, 13);
+            this.LDni.TabIndex = 0;
+            this.LDni.Text = "Dni";
+            // 
+            // LNombre
+            // 
+            this.LNombre.AutoSize = true;
+            this.LNombre.Location = new System.Drawing.Point(38, 179);
+            this.LNombre.Name = "LNombre";
+            this.LNombre.Size = new System.Drawing.Size(44, 13);
+            this.LNombre.TabIndex = 1;
+            this.LNombre.Text = "Nombre";
+            // 
+            // LApellido
+            // 
+            this.LApellido.AutoSize = true;
+            this.LApellido.Location = new System.Drawing.Point(38, 125);
+            this.LApellido.Name = "LApellido";
+            this.LApellido.Size = new System.Drawing.Size(44, 13);
+            this.LApellido.TabIndex = 2;
+            this.LApellido.Text = "Apellido";
+            this.LApellido.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // LNya
+            // 
+            this.LNya.AutoSize = true;
+            this.LNya.Location = new System.Drawing.Point(41, 13);
+            this.LNya.Name = "LNya";
+            this.LNya.Size = new System.Drawing.Size(95, 13);
+            this.LNya.TabIndex = 3;
+            this.LNya.Text = "Nombre y Apellido:";
+            this.LNya.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // LModificar
+            // 
+            this.LModificar.AutoSize = true;
+            this.LModificar.ForeColor = System.Drawing.Color.Red;
+            this.LModificar.Location = new System.Drawing.Point(161, 13);
+            this.LModificar.Name = "LModificar";
+            this.LModificar.Size = new System.Drawing.Size(50, 13);
+            this.LModificar.TabIndex = 4;
+            this.LModificar.Text = "Modificar";
+            // 
+            // TDni
+            // 
+            this.TDni.Location = new System.Drawing.Point(114, 62);
+            this.TDni.Name = "TDni";
+            this.TDni.Size = new System.Drawing.Size(100, 20);
+            this.TDni.TabIndex = 5;
+            this.TDni.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.TDni.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TDni_KeyPress);
+            // 
+            // TApellido
+            // 
+            this.TApellido.Location = new System.Drawing.Point(114, 118);
+            this.TApellido.Name = "TApellido";
+            this.TApellido.Size = new System.Drawing.Size(100, 20);
+            this.TApellido.TabIndex = 6;
+            this.TApellido.TextChanged += new System.EventHandler(this.TApellido_TextChanged);
+            this.TApellido.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TApellido_KeyPress);
+            // 
+            // TNombre
+            // 
+            this.TNombre.Location = new System.Drawing.Point(114, 172);
+            this.TNombre.Name = "TNombre";
+            this.TNombre.Size = new System.Drawing.Size(100, 20);
+            this.TNombre.TabIndex = 7;
+            this.TNombre.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TNombre_KeyPress);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.panel1.Controls.Add(this.checkBox3);
+            this.panel1.Controls.Add(this.checkBox2);
+            this.panel1.Controls.Add(this.checkBox1);
+            this.panel1.Controls.Add(this.LTarjeta);
+            this.panel1.Controls.Add(this.TTelefono);
+            this.panel1.Controls.Add(this.LTelefono);
+            this.panel1.Controls.Add(this.LNya);
+            this.panel1.Controls.Add(this.LDni);
+            this.panel1.Controls.Add(this.LNombre);
+            this.panel1.Controls.Add(this.TNombre);
+            this.panel1.Controls.Add(this.LApellido);
+            this.panel1.Controls.Add(this.TApellido);
+            this.panel1.Controls.Add(this.LModificar);
+            this.panel1.Controls.Add(this.TDni);
+            this.panel1.Location = new System.Drawing.Point(39, 48);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(305, 350);
+            this.panel1.TabIndex = 10;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(164, 320);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(79, 17);
+            this.checkBox3.TabIndex = 13;
+            this.checkBox3.Text = "Mastercard";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(164, 296);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(46, 17);
+            this.checkBox2.TabIndex = 12;
+            this.checkBox2.Text = "Visa";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(164, 272);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(63, 17);
+            this.checkBox1.TabIndex = 11;
+            this.checkBox1.Text = "Naranja";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // LTarjeta
+            // 
+            this.LTarjeta.AutoSize = true;
+            this.LTarjeta.Location = new System.Drawing.Point(41, 277);
+            this.LTarjeta.Name = "LTarjeta";
+            this.LTarjeta.Size = new System.Drawing.Size(96, 13);
+            this.LTarjeta.TabIndex = 10;
+            this.LTarjeta.Text = "Tarjetas de Crédito";
+            // 
+            // TTelefono
+            // 
+            this.TTelefono.Location = new System.Drawing.Point(114, 230);
+            this.TTelefono.Name = "TTelefono";
+            this.TTelefono.Size = new System.Drawing.Size(100, 20);
+            this.TTelefono.TabIndex = 9;
+            this.TTelefono.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TTelefono_KeyPress);
+            // 
+            // LTelefono
+            // 
+            this.LTelefono.AutoSize = true;
+            this.LTelefono.Location = new System.Drawing.Point(41, 233);
+            this.LTelefono.Name = "LTelefono";
+            this.LTelefono.Size = new System.Drawing.Size(49, 13);
+            this.LTelefono.TabIndex = 8;
+            this.LTelefono.Text = "Telefono";
+            // 
+            // RBVaron
+            // 
+            this.RBVaron.AutoSize = true;
+            this.RBVaron.Checked = true;
+            this.RBVaron.Location = new System.Drawing.Point(407, 196);
+            this.RBVaron.Name = "RBVaron";
+            this.RBVaron.Size = new System.Drawing.Size(53, 17);
+            this.RBVaron.TabIndex = 12;
+            this.RBVaron.TabStop = true;
+            this.RBVaron.Text = "Varon";
+            this.RBVaron.UseVisualStyleBackColor = true;
+            this.RBVaron.CheckedChanged += new System.EventHandler(this.RBVaron_CheckedChanged);
+            // 
+            // RBMujer
+            // 
+            this.RBMujer.AutoSize = true;
+            this.RBMujer.Location = new System.Drawing.Point(490, 196);
+            this.RBMujer.Name = "RBMujer";
+            this.RBMujer.Size = new System.Drawing.Size(51, 17);
+            this.RBMujer.TabIndex = 13;
+            this.RBMujer.Text = "Mujer";
+            this.RBMujer.UseVisualStyleBackColor = true;
+            this.RBMujer.CheckedChanged += new System.EventHandler(this.RBMujer_CheckedChanged);
+            // 
+            // Bsalir
+            // 
+            this.Bsalir.Image = global::Proyecto3._0.Properties.Resources.salir;
+            this.Bsalir.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Bsalir.Location = new System.Drawing.Point(427, 423);
+            this.Bsalir.Name = "Bsalir";
+            this.Bsalir.Size = new System.Drawing.Size(89, 50);
+            this.Bsalir.TabIndex = 14;
+            this.Bsalir.Text = "Salir";
+            this.Bsalir.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.Bsalir.UseVisualStyleBackColor = true;
+            this.Bsalir.Click += new System.EventHandler(this.Bsalir_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::Proyecto3._0.Properties.Resources.hombre;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox1.Location = new System.Drawing.Point(407, 48);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(132, 109);
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // BEliminar
+            // 
+            this.BEliminar.Image = global::Proyecto3._0.Properties.Resources.eliminar;
+            this.BEliminar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BEliminar.Location = new System.Drawing.Point(219, 423);
+            this.BEliminar.Name = "BEliminar";
+            this.BEliminar.Size = new System.Drawing.Size(85, 50);
+            this.BEliminar.TabIndex = 9;
+            this.BEliminar.Text = "Eliminar";
+            this.BEliminar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.BEliminar.UseVisualStyleBackColor = true;
+            this.BEliminar.Click += new System.EventHandler(this.BEliminar_Click);
+            // 
+            // BGuardar
+            // 
+            this.BGuardar.Image = global::Proyecto3._0.Properties.Resources.guardar;
+            this.BGuardar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BGuardar.Location = new System.Drawing.Point(80, 423);
+            this.BGuardar.Name = "BGuardar";
+            this.BGuardar.Size = new System.Drawing.Size(106, 50);
+            this.BGuardar.TabIndex = 8;
+            this.BGuardar.Text = "Guardar";
+            this.BGuardar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.BGuardar.UseVisualStyleBackColor = true;
+            this.BGuardar.Click += new System.EventHandler(this.BGuardar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label1.Location = new System.Drawing.Point(214, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 27);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Nuevo Cliente";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(579, 493);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Bsalir);
+            this.Controls.Add(this.RBMujer);
+            this.Controls.Add(this.RBVaron);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.BEliminar);
+            this.Controls.Add(this.BGuardar);
+            this.Name = "Form1";
+            this.Text = "Pequeño Formulario";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LDni;
+        private System.Windows.Forms.Label LNombre;
+        private System.Windows.Forms.Label LApellido;
+        private System.Windows.Forms.Label LNya;
+        private System.Windows.Forms.Label LModificar;
+        private System.Windows.Forms.TextBox TDni;
+        private System.Windows.Forms.TextBox TApellido;
+        private System.Windows.Forms.TextBox TNombre;
+        private System.Windows.Forms.Button BGuardar;
+        private System.Windows.Forms.Button BEliminar;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label LTarjeta;
+        private System.Windows.Forms.TextBox TTelefono;
+        private System.Windows.Forms.Label LTelefono;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RadioButton RBVaron;
+        private System.Windows.Forms.RadioButton RBMujer;
+        private System.Windows.Forms.Button Bsalir;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
